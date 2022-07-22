6587f33f46a74032516688e2e8b4e4ee05a806ad
radha30ramai <190020116037ait@gmail.com>
2022-07-22 17:23:17 +0530

    this is first

---
+++ ForExample.java
@@ -0,0 +1,12 @@
+//java program to demonstrate the example of for loop
+//which prints table of 1
+public class ForExample
+{
+	public static void main(String args[])
+	{
+		for (int i=1;i<=10;i++)
+		{
+			System .out.println(i);
+		}
+	}
+}
