bfadf225159b886c6bfa16c7c7134ede3e4fea3d
radha30ramai <190020116037ait@gmail.com>
2022-07-22 17:47:26 +0530

    this is first

--- ForExample.java
+++ ForExample.java
@@ -1,12 +1,16 @@
 //java program to demonstrate the example of for loop
 //which prints table of 1
-public class ForExample
+class PyramidExample
 {
 	public static void main(String args[])
 	{
-		for (int i=1;i<=10;i++)
+		for(int i = 1; i <=5; i++)
 		{
-			System .out.println(i);
+			for(int j = 1; j <=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
 	}
-}
+}		
--- PyramidExample.java
+++ PyramidExample.java
@@ -1,16 +1,18 @@
 //to find pyramid
-public class PyramidExample
+class PyramidExample
 {
-	public static void main("String args[]")
+	public static void main(String args[])
 	{
-		for int (i=1;i<=5;i++);
-		for int (j=1;j<=i;j++);
+		for(int i = 1; i <=5; i++)
 		{
-			System.out.print(" * ");
+			for(int j = 1; j <=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
-}
+}		
 
 
 		
