21a0bb03316c5edb9e39e5efd73ae927d2541200
radha30ramai <190020116037ait@gmail.com>
2022-07-22 12:17:49 +0530

    This is first

---
+++ DataType.java
@@ -0,0 +1,26 @@
+class DataType
+{
+	public static void main(String args[])
+	{
+		int num1=150;
+		short myShort=500;
+		byte myByte=40;
+		long myLong=10000000L;
+		float myFloat=26.7f;
+		double myDouble=23.998888898;
+		char myChar='a';
+		boolean myBool=false;
+		System.out.println("Integer number is:"+num1);
+		System.out.println("Short Integer number is:"+myShort);
+		System.out.println("Byte Integer number is:"+myByte);
+		System.out.println("Long Integer number is:"+myLong);
+		System.out.println("Floating point number is:"+myFloat);
+		System.out.println("Double floating number is:"+myDouble);
+		System.out.println("My first character is:"+myChar);
+		if(myBool==false)
+		{
+			System.out.println("My boolean data is:"+myBool);
+		}
+
+	}
+}
