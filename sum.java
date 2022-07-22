2ef8d608ad15a03d952b37d0b0f36b884161e7ba
radha30ramai <190020116037ait@gmail.com>
2022-07-22 12:50:54 +0530

    this is first

---
+++ sum.java
@@ -0,0 +1,21 @@
+// write a java program that accepts two integers and then print sum, diiffrence,  product and average of those numbers.
+
+class sum
+{
+	public static void main(String args[])
+	{
+		int num1,num2,sum,diff,avg,product;
+		num1=10;
+		num2=20;
+		sum=num1+num2;//to find sum
+		diff=num1-num2;//to find diiffrence
+		avg=sum/2;//to find average
+		product=num1*num2;//to find product
+		System.out.println("sum of="+sum);
+		System.out.println("sum of="+diff);
+		System.out.println("sum of="+avg);
+		System.out.println("sum of="+product);
+		
+	}
+}
+	
