78cf4976b9d9c29f9c996eca7ffdab4e63081238
radha30ramai <190020116037ait@gmail.com>
2022-07-22 16:24:04 +0530

    this is first

---
+++ PositiveNegativeExample.java
@@ -0,0 +1,20 @@
+//Program to check given number.
+public class PositiveNegativeExample
+{
+	public static void main(String args[])
+	{
+		int number=20;
+		if (number>0)
+		{
+			System.out.println("POSITIVE");
+		}
+		else if (number<0)
+		{
+			System.out.println("NEGATIVE");
+		}
+		else
+		{
+			System.out.println("ZERO");
+		}
+	}
+}
