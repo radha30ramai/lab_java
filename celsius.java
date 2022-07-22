6cd6d7e85a316921a8fcd1cc0b13d1698d3a1487
radha30ramai <190020116037ait@gmail.com>
2022-07-22 12:58:18 +0530

    this is first

---
+++ celsius.java
@@ -0,0 +1,11 @@
+//write a java program to convert fahrenheit to celsius.
+class celsius
+{
+	public static void main(String args[])
+	{
+		float fahrenheit , celsius;
+		fahrenheit=3.6f;
+		celsius=((fahrenheit-32)*5/9);//to convert celsius
+		System.out.println("temperature in celsius is:"+celsius);
+	}
+}
