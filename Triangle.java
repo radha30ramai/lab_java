b0953529946e1eae86c1dda5a008155e6efbe9e3
radha30ramai <190020116037ait@gmail.com>
2022-07-28 16:39:23 +0530

    thursday lab

---
+++ Triangle.java
@@ -0,0 +1,23 @@
+// Write a Java program to display the pattern like right angle triangle with a number.
+
+import java.util.Scanner;
+public class Triangle 
+{
+
+   public static void main(String args[])
+
+{
+   int i,j,n;
+   System.out.print("Input number of rows : ");
+ Scanner in = new Scanner(System.in);
+		    n = in.nextInt();
+
+   for(i=1;i<=n;i++)
+   {
+	for(j=1;j<=i;j++)
+	  System.out.print(j);
+
+    System.out.println("");
+    }
+}
+} 
